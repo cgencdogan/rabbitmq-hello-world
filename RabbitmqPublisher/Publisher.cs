9d71e674dd8bf45b655b473fe52671f26de7fd2c
Caner GENÇDOĞAN <gencdogancaner@gmail.com>
2023-07-02 21:45:44 +0300

    Add project files.

---
+++ RabbitmqPublisher/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+string message;
+byte[] body;
+
+message = Console.ReadLine();
+
+while (message!="stop")
+{
+    body = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: body);
+    Console.WriteLine($" [x] Sent {message}");
+    Console.WriteLine(" Type [stop] to stop program.");
+    message = Console.ReadLine();
+}
